f143e1ca8353d8a265e1e6e0429258c88e63af6d
Henri Kotkanen <henri.kotkanen@gmail.com>
2017-07-20 17:23:07 +0300

    small deployment related changes

--- touch_helsinki/settings.py
+++ touch_helsinki/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 
 # local_settings.py can be used to override environment-specific settings
